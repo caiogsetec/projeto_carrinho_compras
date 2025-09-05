6dd75f6d9deb2b3747ec5a02361bb7b12146d039
caiogsetec <143644880+caiogsetec@users.noreply.github.com>
2025-09-05 20:21:53 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once './src/Product.php';
+require_once './src/Cart.php';
+
+$products = [
+    new Product(1, "Camisa", 59.90, 10),
+    new Product(2, "Short Jeans", 129.90, 5),
+    new Product(3, "Tênis", 199.90, 3),
+];
+
+$cart = new Cart();
+
+echo $cart->addItem($products[0], 2);
+echo $cart->addItem($products[2], 10);
+$cart->listItems();
+
+echo $cart->removeItem(1);
+$cart->listItems();
+
+echo $cart->addItem($products[1], 1);
+$cart->listItems();
+
+echo $cart->applyDiscount("MENOS15");
+echo "Total final: \$" . $cart->calculateTotal() . "\n";
+
